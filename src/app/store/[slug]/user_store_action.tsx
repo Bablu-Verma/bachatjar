31f11ab2c068838f28af6cb3e36cb30ab8e5a644
Bablu verma <jbabluverma@gmail.com>
2025-07-15 11:38:39 +0530

    add changes and remove extra fileds

--- src/app/store/[slug]/user_store_action.tsx
+++ src/app/store/[slug]/user_store_action.tsx
@@ -19,7 +19,7 @@ const UserStoreAction: React.FC<UserStoreActionProps> = ({ store_ }) => {
   const token = useSelector((state: RootState) => state.user.token);
   const user = useSelector((state: RootState) => state.user.user);
 
-  
+
 
   const shop_now = async () => {
     if (!token) {
@@ -32,16 +32,20 @@ const UserStoreAction: React.FC<UserStoreActionProps> = ({ store_ }) => {
       return;
     }
 
-   
-   const storeId = store_._id;
-   window.open(`/create-order?store_id=${storeId}`, '_blank');
+    if (store_.store_type != 'NON_INSENTIVE') {
+      const storeId = store_._id;
+      window.open(`/create-order?store_id=${storeId}`, '_blank');
+    } else {
+      window.open(store_.store_link, '_blank');
+    }
 
 
-    
+
+
   };
 
 
-  const create_share_link = ()=> {
+  const create_share_link = () => {
     if (!token) {
       toast.error("You need to login to proceed");
 
@@ -52,18 +56,20 @@ const UserStoreAction: React.FC<UserStoreActionProps> = ({ store_ }) => {
       return;
     }
 
+
+
     const create_link = `${process.env.NEXT_PUBLIC_SITE_URL}/create-share-order?store_id=${store_._id}&user_id=${user?._id}`
 
     navigator.clipboard
-    .writeText(create_link)
-    .then(() => {
-      toast.success("Link copied to clipboard!");
-    })
-    .catch((err) => {
-      console.error("Failed to copy: ", err);
-      toast.error("Failed to copy link. Please try again.");
-    });
-    
+      .writeText(create_link)
+      .then(() => {
+        toast.success("Link copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy: ", err);
+        toast.error("Failed to copy link. Please try again.");
+      });
+
   }
 
 
@@ -76,21 +82,21 @@ const UserStoreAction: React.FC<UserStoreActionProps> = ({ store_ }) => {
           className="border-[1px] cursor-pointer justify-center items-center gap-2 inline-flex text-base rounded px-6 py-2 text-white "
         >
           <span> {
-            store_.store_type != 'NON_INSENTIVE' ? 'Shop & Earn': 'Shop & Save'
-            } </span>
+            store_.store_type != 'NON_INSENTIVE' ? 'Shop & Earn' : 'Shop & Save'
+          } </span>
           <FaExternalLinkAlt />
         </a>
         {
-          store_.store_type != 'NON_INSENTIVE' &&  <button
-          type="button"
-          onClick={create_share_link}
-          className=" justify-center items-center gap-2 text-base border-[1px] border-green-400 text-secondary inline-flex px-6 py-2 bg-green-400 rounded"
-        >
-          <span>Share Link</span>
-          <IoMdShare className="text-lg" />
-        </button>
+          store_.store_type != 'NON_INSENTIVE' && <button
+            type="button"
+            onClick={create_share_link}
+            className=" justify-center items-center gap-2 text-base border-[1px] border-green-400 text-secondary inline-flex px-6 py-2 bg-green-400 rounded"
+          >
+            <span>Share Link</span>
+            <IoMdShare className="text-lg" />
+          </button>
         }
-       
+
       </div>
     </>
   );
